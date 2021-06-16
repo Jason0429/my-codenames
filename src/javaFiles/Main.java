3323f79dc95d748b48e70b86833ffde1f7b95761
Jason Cheung <jasoncheung0429@gmail.com>
2021-06-16 00:12:48 -0400

    Added javaFiles

--- src/javaFiles/Main.java
+++ src/javaFiles/Main.java
@@ -1,15 +1,70 @@
 package javaFiles;
 
 import java.util.ArrayList;
-
-// JSONParser
-import org.json.simple.*;
-import org.json.simple.parser.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Word[] wordsArray = new Word[25];
-        ArrayList<Word> wordsArrayList = new ArrayList<Word>();
-        
+        Requirements r = new Requirements();
+
+        String[] w1a = { "horseshoe", "mother", "drop", "sahara", "ski", "church", "map", "file", "glass", "eagle",
+                "racket", "spoon", "parade", "whale", "march", "tiger", "undertaker", "sound", "cloak", "mess", "robin",
+                "buck", "blues", "mug", "wheelchair" };
+        ArrayList<String> w1 = new ArrayList<String>();
+        arrayToArrayList(w1a, w1);
+
+        Word[] wordsArray1 = new Word[25];
+        wordsArray1 = r.populateWordArray(wordsArray1, 0, w1.size(), w1);
+
+        drawLine();
+        System.out.println("Array of Words before mergeSort");
+        drawLine();
+        r.traverseArray(wordsArray1, 0);
+        drawLine();
+        System.out.println("Array of Words after mergeSort");
+        drawLine();
+        r.mergeSort(wordsArray1, 0, wordsArray1.length - 1);
+        r.traverseArray(wordsArray1, 0);
+        drawLine();
+
+        ArrayList<Word> wordsArrayList1 = new ArrayList<Word>();
+        w1 = new ArrayList<String>();
+        arrayToArrayList(w1a, w1);
+        wordsArrayList1 = r.populateWordArrayList(wordsArrayList1, 0, w1.size(), w1);
+        drawLine();
+        System.out.println("ArrayList of Words before mergeSort");
+        drawLine();
+        r.traverseArrayList(wordsArrayList1, 0);
+        drawLine();
+        System.out.println("ArrayList of Words after mergeSort");
+        drawLine();
+        r.mergeSort(wordsArrayList1, 0, wordsArrayList1.size() - 1);
+        r.traverseArrayList(wordsArrayList1, 0);
+        drawLine();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Binary Search");
+        System.out.println("Please enter a word to search for:");
+        String value = sc.nextLine();
+        int result = r.binarySearch(wordsArray1, 0, wordsArray1.length, value);
+        System.out.println(result == -1 ? "Sorry your word was not found." : value + " found at index " + result);
+        sc.close();
+
+    }
+
+    public static void arrayToArrayList(String[] arr, ArrayList<String> arrList) {
+        for (String s : arr) {
+            arrList.add(s);
+        }
+    }
+
+    public static void drawLine() {
+        System.out.println("=================================");
+    }
+
+    public static void printArray(Word[] arr) {
+        for (Word w : arr) {
+            System.out.println(w);
+        }
     }
 }
--- src/javaFiles/Requirements.java
+++ src/javaFiles/Requirements.java
@@ -1,15 +1,9 @@
 package javaFiles;
 
+import java.util.ArrayList;
+
 public class Requirements {
-    private final String WORDS_JSON = getClass().getResource("words.json").toString();
-
-    public String printWordsJSON() {
-        return this.WORDS_JSON;
-    }
-
     public Word[] populateWordArray(Word[] arr, int i, int length, ArrayList<String> words) {
-        // Add 25 words to words arraylist
-
         if (i == length) {
             return arr;
         }
@@ -22,18 +16,16 @@ public class Requirements {
             arr[i] = new RedWord(words.remove(0));
         } else if (i < 17) {
             arr[i] = new BlueWord(words.remove(0));
-        } else if (i < 24) {
-            arr[i] = new WhiteWord(words.remove(0));
-        } else {
+        } else if (i < 18) {
             arr[i] = new BlackWord(words.remove(0));
+        } else {
+            arr[i] = new WhiteWord(words.remove(0));
         }
 
-        return populateWordArray(arr, ++i, length);
+        return populateWordArray(arr, ++i, length, words);
     }
 
     public ArrayList<Word> populateWordArrayList(ArrayList<Word> arr, int i, int length, ArrayList<String> words) {
-        // Add 25 words to words arraylist
-
         if (i == length) {
             return arr;
         }
@@ -46,10 +38,10 @@ public class Requirements {
             arr.add(new RedWord(words.remove(0)));
         } else if (i < 17) {
             arr.add(new BlueWord(words.remove(0)));
-        } else if (i < 24) {
-            arr.add(new WhiteWord(words.remove(0)));
-        } else {
+        } else if (i < 18) {
             arr.add(new BlackWord(words.remove(0)));
+        } else {
+            arr.add(new WhiteWord(words.remove(0)));
         }
 
         return populateWordArrayList(arr, ++i, length, words);
@@ -57,9 +49,9 @@ public class Requirements {
 
     // Recursively traverses an array
     // i must start at 0
-    public String traverseArray(Word[] arr, int i) {
+    public boolean traverseArray(Word[] arr, int i) {
         if (i == arr.length) {
-            return null;
+            return true;
         }
 
         System.out.println(arr[i]);
@@ -69,21 +61,151 @@ public class Requirements {
 
     // Recursively traverses an ArrayList
     // i must start at 0
-    public String traverseArrayList(ArrayList<Word> arr, int i) {
+    public boolean traverseArrayList(ArrayList<Word> arr, int i) {
         if (i == arr.size()) {
-            return null;
+            return true;
         }
 
         System.out.println(arr.get(i));
 
-        return traverseArray(arr, ++i);
+        return traverseArrayList(arr, ++i);
+    }
+
+    // Merge helper method (ArrayList)
+    public void merge(ArrayList<Word> arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        ArrayList<Word> L = new ArrayList<Word>();
+        for (int i = 0; i < n1; i++) {
+            L.add(null);
+        }
+        ArrayList<Word> R = new ArrayList<Word>();
+        for (int i = 0; i < n2; i++) {
+            R.add(null);
+        }
+
+        for (int i = 0; i < n1; ++i)
+            L.set(i, arr.get(l + i));
+        for (int j = 0; j < n2; ++j)
+            R.set(j, arr.get(m + 1 + j));
+
+        int i = 0;
+        int j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L.get(i).getWord().compareTo(R.get(j).getWord()) <= 0) {
+                arr.set(k, L.get(i));
+                i++;
+            } else {
+                arr.set(k, R.get(j));
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr.set(k, L.get(i));
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr.set(k, R.get(j));
+            j++;
+            k++;
+        }
+    }
+
+    // Merge helper method (Array)
+    public void merge(Word[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        Word[] L = new Word[n1];
+        Word[] R = new Word[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0;
+        int j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getWord().compareTo(R[j].getWord()) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
     // Merge Sort (Array)
+    public void mergeSort(Word[] arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
 
     // Merge Sort (ArrayList)
+    public void mergeSort(ArrayList<Word> arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
 
     // Binary Search
+    public int binarySearch(Word[] arr, int l, int r, String value) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
 
+            // If found
+            if (arr[mid].getWord().equals(value))
+                return mid;
 
+            if (arr[mid].getWord().compareTo(value) > 0)
+                return binarySearch(arr, l, mid - 1, value);
+
+            return binarySearch(arr, mid + 1, r, value);
+        }
+
+        // If not found
+        return -1;
+    }
 }
